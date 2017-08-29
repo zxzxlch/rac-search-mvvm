658ca32b54cec532f6ff14fc2784bcaac7bca000
Benjamin C <zxzxlch@gmail.com>
2017-08-29 16:14:41 +0800

    Initial commit

---
+++ rac-search-mvvm/SearchTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchTableViewController.swift
+//  learn-reactivecocoa
+//
+//  Created by Benjamin C on 29/8/17.
+//  Copyright © 2017 Benjamin Cheah. All rights reserved.
+//
+
+import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+
+class SearchTableViewController: UITableViewController {
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 320, height: 44))
+        searchBar.placeholder = "Search places"
+        searchBar.showsCancelButton = true
+        return searchBar
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.titleView = searchBar
+        
+        setupBindings()
+    }
+    
+    func setupBindings() {
+        
+    }
+    
+}
